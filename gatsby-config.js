18e0e4b21bd735d705ae2580d4b33342b4a5bd9c
Simon Lerpiniere <simon@lms.uk.net>
2022-01-24 16:00:02 +0000

    Finished Gatsby Tutorial Part 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`
   },
   plugins: [
-    "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+    "gatsby-plugin-mdx", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"
