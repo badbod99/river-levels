0fa5b5940fb1f0d8eb975ba5b2b6feac64abdfaf
Simon Lerpiniere <simon@lms.uk.net>
2023-02-13 16:09:26 +0000

    Adding fonts and icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
-function App() {
+function App() {;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Button variant="contained">Hello World</Button>
+        <DeleteIcon />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
